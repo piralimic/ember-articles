c964ea9de12663eed81279355b69634f6eda8b82
piralimic <pirali.michael@gmail.com>
2020-02-24 10:28:44 +0100

    update series.js + series.hbs

--- app/routes/series.js
+++ app/routes/series.js
@@ -1,4 +1,7 @@
-import Route from '@ember/routing/route';
+import Route from '@ember/routing/route'
 
-export default class SeriesRoute extends Route {
-}
+export default Route.extend({
+  model: function() {
+    return this.store.find("seriesItem");
+  },
+});
